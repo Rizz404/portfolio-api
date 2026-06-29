064d6060d59e1c591006ec6a90a9b697dd2ea885
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2026-06-29 15:47:34 +0700

    feat(repository): add all repository

---
+++ src/main/java/com/api/rizz/portfolio_api/repository/BlogAttachmentRepository.java
@@ -0,0 +1,14 @@
+package com.api.rizz.portfolio_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.rizz.portfolio_api.entity.BlogAttachment;
+
+@Repository
+/**
+ * BlogAttachmentRepository
+ */
+public interface BlogAttachmentRepository extends JpaRepository<BlogAttachment, Long> {
+
+}
---
+++ src/main/java/com/api/rizz/portfolio_api/repository/ExperienceRepository.java
@@ -0,0 +1,14 @@
+package com.api.rizz.portfolio_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.rizz.portfolio_api.entity.Experience;
+
+@Repository
+/**
+ * ExperienceRepository
+ */
+public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+
+}
