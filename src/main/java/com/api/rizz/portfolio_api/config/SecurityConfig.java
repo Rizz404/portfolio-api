6fff69a960d3fc14862012ca8842b875687f9faf
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2026-07-29 14:14:23 +0700

    feat(config): implement CORS configuration in SecurityConfig for enhanced security

--- src/main/java/com/api/rizz/portfolio_api/config/SecurityConfig.java
+++ src/main/java/com/api/rizz/portfolio_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.api.rizz.portfolio_api.config;
 
 import com.api.rizz.portfolio_api.security.JwtAuthFilter;
 import com.api.rizz.portfolio_api.service.UserDetailServiceImpl;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /** SecurityConfig */
 @Configuration
@@ -29,18 +33,41 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .csrf(csrf -> csrf.disable())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         // Mengubah otorisasi endpoint
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .authenticationProvider(authenticationProvider())
-        .addFilterBefore(
-            jwtAuthFilter,
-            UsernamePasswordAuthenticationFilter.class); // Mengizinkan seluruh akses tanpa
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Mengizinkan
+    // seluruh
+    // akses tanpa
     // kredensial login
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+    // * Mengizinkan metode HTTP yang diperlukan
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+    // * Mengizinkan header yang dikirim dari klien (penting untuk JWT)
+    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+    // * Untuk cookie
+    configuration.setAllowCredentials(true);
+
+    // * Menerapkan konfigurasi ini ke semua endpoint (/**)
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 
   @Bean
